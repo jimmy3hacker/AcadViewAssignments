4964af9cf40f8ff79ca7848041c0196653c29fd7
jimmyshalu <sonid1411@gmail.com>
2018-06-14 16:44:01 +0530

    question 2

--- app/src/main/java/com/example/admin/test/MainActivity.java
+++ app/src/main/java/com/example/admin/test/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.admin.test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,58 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast centerToast = null;
+        centerToast.makeText(getApplicationContext(),"this is my toast",Toast.LENGTH_SHORT);
+        centerToast.setGravity(Gravity.CENTER,0,0);
 
-        Toast.makeText(MainActivity.this,"onCreate",Toast.LENGTH_SHORT).show();
+        centerToast.show();
 
         Log.d("test","I am in onCreate()");
-
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        Log.d("test","I am in onStart()");
-        Toast.makeText(MainActivity.this,"onStart",Toast.LENGTH_SHORT).show();
-
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("test","I am in onPause()");
-        Toast.makeText(MainActivity.this,"onPause",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("test","I am in onDestory()");
-        Toast.makeText(MainActivity.this,"onDestroy",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("test","I am in onStop()");
-        Toast.makeText(MainActivity.this,"onStop",Toast.LENGTH_SHORT).show();
-        }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d("test","I am in onRestart()");
-        Toast.makeText(MainActivity.this,"onRestart",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("test","I am in onResume()");
-        Toast.makeText(MainActivity.this,"onResume",Toast.LENGTH_SHORT).show();
-
-
 
     }
 }
