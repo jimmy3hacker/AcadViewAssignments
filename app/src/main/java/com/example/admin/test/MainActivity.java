6738ea3f80692f1344abb0e38c58556f4e74a0ec
jimmyshalu <sonid1411@gmail.com>
2018-06-14 16:26:19 +0530

    Question 1

--- app/src/main/java/com/example/admin/test/MainActivity.java
+++ app/src/main/java/com/example/admin/test/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(MainActivity.this,"THis is my first toast",Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this,"onCreate",Toast.LENGTH_SHORT).show();
 
         Log.d("test","I am in onCreate()");
 
@@ -22,13 +22,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        Log.d("test","I am in onCreate()");
+        Log.d("test","I am in onStart()");
+        Toast.makeText(MainActivity.this,"onStart",Toast.LENGTH_SHORT).show();
 
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("test","I am in onPause()");
+        Toast.makeText(MainActivity.this,"onPause",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("test","I am in onDestory()");
+        Toast.makeText(MainActivity.this,"onDestroy",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("test","I am in onStop()");
+        Toast.makeText(MainActivity.this,"onStop",Toast.LENGTH_SHORT).show();
+        }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("test","I am in onRestart()");
+        Toast.makeText(MainActivity.this,"onRestart",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d("test","I am in onResume()");
+        Toast.makeText(MainActivity.this,"onResume",Toast.LENGTH_SHORT).show();
 
 
 
